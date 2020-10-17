f80f21fc9ef1876eaa6b41efb026c046aa0eb1ad
Misha Oleinychenko <m.oleinychenko@gmail.com>
2020-10-17 15:05:46 -0400

    week 4 v-3 final

--- js/acco.js
+++
@@ -1,8 +0,0 @@
-$(".team__title").on("click", function (e) {
-  e.preventDefault();
-  $this = $(e.currentTarget);
-});
-
-// !logic
-// todo 1. process event and assign to this
-// todo 1. func to open
--- js/carousel.js
+++ js/carousel.js
@@ -1,36 +1,12 @@
-let arrowRight = document.querySelectorAll(".slider__image--svg--right");
-let arrowLeft = document.querySelectorAll(".slider__image--svg--left");
-let slideWidth = document.querySelector(".slider__item");
-let sliderRoot = document.getElementById("sliderRoot");
-let sliderList = document.querySelector(".slider__list");
-
-let sliderPosition = 0;
-
-arrowRight.forEach((arrow) => {
-  arrow.addEventListener("click", () => moveRight());
+const slider = $(".slider__list").bxSlider({
+  pager: false,
+  controls: false,
 });
-arrowLeft.forEach((arrow) => {
-  arrow.addEventListener("click", () => moveLeft());
+$(".slider__image--svg--left").on("click", function () {
+  slider.goToPrevSlide();
+});
+$(".slider__image--svg--right").on("click", function () {
+  slider.goToNextSlide();
 });
 
-// if we at last slide -> it resets to original
-function moveRight() {
-  sliderPosition += slideWidth.clientWidth;
-  if (sliderPosition === sliderList.scrollWidth) {
-    sliderPosition = 0;
-  }
-  sliderRoot.style.transform = `translate(-${sliderPosition}px)`;
-}
-
-function moveLeft() {
-  if (sliderPosition === 0) {
-    sliderPosition = sliderList.scrollWidth - slideWidth.clientWidth;
-  } else {
-    sliderPosition -= slideWidth.clientWidth;
-  }
-
-  sliderRoot.style.transform = `translate(-${sliderPosition}px)`;
-}
-
-sliderList.addEventListener("swiped-right", () => moveLeft());
-sliderList.addEventListener("swiped-left", () => moveRight());
+// * https://bxslider.com/options/
